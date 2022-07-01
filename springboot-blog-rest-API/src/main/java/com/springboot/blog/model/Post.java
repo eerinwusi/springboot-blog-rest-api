68a9d4f22af4910a7ccf62afa8cae819396c29eb
eerinwusi <63309802+eerinwusi@users.noreply.github.com>
2022-07-01 11:01:47 +0100

    springboot-blog-rest-api

---
+++ springboot-blog-rest-API/src/main/java/com/springboot/blog/model/Post.java
@@ -0,0 +1,25 @@
+package com.springboot.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = "title")})
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private String content;
+}
